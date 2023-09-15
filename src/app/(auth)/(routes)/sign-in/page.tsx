7da19a45c4007e637b29b0a500175e422012720c
mauro-balades <mauro.balades@tutanota.com>
2023-09-15 17:56:30 +0200

    Added sign-in page too

--- src/app/(auth)/(routes)/sign-in/page.tsx
+++ src/app/(auth)/(routes)/sign-in/page.tsx
@@ -16,12 +16,12 @@ export default function AuthenticationPage() {
     <>
         <div className="container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-            href="/login"
+            href="/sign-up"
             className={cn(
               "absolute font-bold right-4 top-4 md:right-8 md:top-8"
             )}
         >
-            Login
+            Create account
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
@@ -42,30 +42,13 @@ export default function AuthenticationPage() {
             <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
                 <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                Nice to see you again!
                 </h1>
                 <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Enter your email below to sign into your account
                 </p>
             </div>
             <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-                By clicking continue, you agree to our{" "}
-                <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-                >
-                Terms of Service
-                </Link>{" "}
-                and{" "}
-                <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-                >
-                Privacy Policy
-                </Link>
-                .
-            </p>
             </div>
         </div>
         </div>
