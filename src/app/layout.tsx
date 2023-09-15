9d3aca125aef33d388144f70db286673e9be2d36
mauro-balades <mauro.balades@tutanota.com>
2023-09-15 21:24:09 +0200

    Started basic prisma db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className="dark" lang="en">
+    <html className="dark" lang="en" suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
     </html>
   );
