57c876b644deca81b4c8ace4b3f3eff1e33c4a83
mauro-balades <mauro.balades@tutanota.com>
2023-10-03 18:21:16 +0200

    Disabled ESLint

--- next.config.js
+++ next.config.js
@@ -10,7 +10,12 @@ const nextConfig = {
         });
 
         return config;
-    }
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
